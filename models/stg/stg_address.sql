20ee485a5d33fd42981843604c9daaf697870f3f
mick.groth.2022@anderson.ucla.edu <mick.groth.2022@anderson.ucla.edu>
2022-02-16 02:35:01 +0000

    added stg

---
+++ models/stg/stg_address.sql
@@ -0,0 +1,5 @@
+
+{{ config(materialized='table') }}
+
+select * from {{ source('mysql_rds_sakila', 'address') }}
+where _fivetran_deleted = FALSE
---
+++ models/stg/stg_store.sql
@@ -0,0 +1,5 @@
+
+{{ config(materialized='table') }}
+
+select * from {{ source('mysql_rds_sakila', 'store') }}
+where _fivetran_deleted = FALSE
