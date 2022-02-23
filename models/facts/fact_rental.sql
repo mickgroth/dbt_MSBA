9eafdef64969e1017c3c06acec90abd915788afc
mick.groth.2022@anderson.ucla.edu <mick.groth.2022@anderson.ucla.edu>
2022-02-23 03:02:18 +0000

    created type2 customer and updated fact table

--- models/facts/fact_rental.sql
+++ models/facts/fact_rental.sql
@@ -3,9 +3,9 @@
 
 SELECT 
     stg_rental.rental_id,
-    dim_customer.customer_key,
+    dim_customer_type_two.customer_key,
     dim_staff.staff_key,
     1 as rental_quantity
 FROM {{ ref('stg_rental') }}
-LEFT JOIN {{ ref('dim_customer') }} ON stg_rental.customer_id = dim_customer.customer_id
+LEFT JOIN {{ ref('dim_customer_type_two') }} ON stg_rental.customer_id = dim_customer_type_two.customer_id
 LEFT JOIN {{ ref('dim_staff') }} ON stg_rental.staff_id = dim_staff.staff_id
